71f26af386a1b8b59fc6a0ccd50a58db2db16cae
Muhammad Junayed <u2008023@student.cuet.ac.bd>
2025-03-24 03:51:54 +0600

    Update Ask a doctor.php

--- Ask a doctor.php
+++ Ask a doctor.php
@@ -210,59 +210,12 @@ function scrollToTop(){
 
 
 
-<footer>
-        <div class="row">
-            <div class="col">
-                <a href="Aboutus.php">
-                <img src="./image/icon.png"class=logofoot height="50" weidth="40">
-                <p>Visit Aroma Pharmacy</p><p>To unlock a Healthier you </p>                </a>
-                <div class="social-icons">
-          
-             <span class="itwo"><i class="bx bxl-whatsapp"></i></span>
-              <span class="ithree"><i class="bx bxl-messenger"></i></span>
-                          <span class="ione"><i class="bx bxl-facebook-circle"></i></span>
-              <span class="ifour"><i class="bx bxl-instagram-alt"></i></span>
-            </div>
-            </div>
-            <div class="col">
-            <h3>Our Details <div class="underline"><span></span></div></h3>
-  
-            <p class="email-id"><a href="mailto:u2008023@student.cuet.ac.bd">u2008023@student.cuet.ac.bd</a></p>
-            <h4><p>+880-1876220119</p></h4>
-   
-            </div>
-    
-            <div class="col">
-                <h3>Links <div class="underline"><span></span></div></h3>
-                <ul>
-                    <li><a href="index.php"><p></p>Home</a></li>
-                    <li><a href="shop.php">Product & Services</a></li>
-                    <li><a href="Ask a doctor.php">Ask a doctor</a></li>
+<?php
 
-                    <li><a href="Article.php">Article</a></li>
-                    <li><a href="contactus.php">  Contact </a></li>
-                    
-                </ul>
-            </div>
-            <div class="col">
-            <h3>Health Article<div class="underline"><span></span></div></h3>
-            <form class="news">
-              
-                <i class='bx bxs-envelope' id="one"></i>
-                <input type="email" placeholder="Enter your email id" required>
-                <button><i class='bx bx-right-arrow-alt' id="two"></i></button>
-            </form>
-            <p class="pay">Secured Payment Gateways</p>
-            <div class="pay2">
-           
-            </div>
-            </div>
-            
-    
-        </div>
-        <hr>
-        <p class="copyright"> &copy2024 Aroma Pharmacy.All Rights Reserved; Designed by  <span class="design"> MUHAMMAD JUNAYED </span>        </p>        </p>
-    </footer>
+
+include("footer.php");
+  ?>
+
 
 
 
